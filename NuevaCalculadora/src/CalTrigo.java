a6a4366be80b93e14a449a6e7236b27682d4e6c7
Estudiantes <Estudiantes@labing50413.udistrital.edu.co>
2018-08-17 11:50:38 -0500

    calculadorAritmetica v1.1

--- NuevaCalculadora/src/CalTrigo.java
+++ NuevaCalculadora/src/CalTrigo.java
@@ -1,3 +1,26 @@
+
+import static java.lang.Math.*;
+
 public class CalTrigo {
+
+    double angGrados = 0;
+
+    void convertirAngulo(double angRadianes) {
+        angGrados = Math.toRadians(angRadianes);
+    }
+
+    double seno() {
+        return sin(angGrados);
+    }
     
+    double coseno() {
+        return cos(angGrados);
+    }
+    
+    double tangente() {
+        return tan(angGrados);
+    }
+    
+    
+
 }
