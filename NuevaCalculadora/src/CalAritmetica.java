e8f2e0e0d71768b0f0570189d13ed1e8e1bb435c
Estudiantes <Estudiantes@labing50413.udistrital.edu.co>
2018-08-17 11:25:50 -0500

    calculadorAritmetica v1.0

---
+++ NuevaCalculadora/src/CalAritmetica.java
@@ -0,0 +1,24 @@
+public class CalAritmetica {
+int operando1;
+int operando2;
+int suma() {
+    int resultado;
+    resultado = operando1 + operando2;
+    return resultado;
+}
+int resta(){
+    int resultado;
+    resultado = operando1 - operando2;
+    return resultado;
+}
+int multiplicacion() {
+    int resultado;
+    resultado = operando1 * operando2;
+    return resultado;
+}
+double division(){
+    double  resultado;
+    resultado = operando1/operando2;
+    return resultado;
+}
+}
